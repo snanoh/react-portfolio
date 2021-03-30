a6b13e8323e237fe181f49bbe458cb53b0bb5df2
User <njs1324@gmail.com>
2021-03-30 21:42:43 +0900

    20210330 first commit

---
+++ src/views/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+
+function MainPage() {
+
+  return (
+    <div>
+        <Container>
+            <h1>test</h1>
+        </Container>
+      
+    </div>
+  );
+}
+export default MainPage
